3c48fd5581eca82c7de5096e7fb92f1376a52515
Satyam Kumar <yaduvanshii092@gmail.com>
2022-07-17 22:49:31 +0530

    deployement init

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import useStyles from './styles';
-import memories from '../../images/memories.png';
+import logo from '../../images/logo.png';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 
@@ -37,8 +37,8 @@ const Navbar = () => {
     return (
     <AppBar className={classes.appBar} position="static" color="inherit">
         <div className={classes.brandContainer}>
-          <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center"> Memories </Typography>
-          <img className={classes.image} src={memories} alt="memories" height="60" />
+          <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center"> Travel Blog </Typography>
+          <img className={classes.image} src={logo} alt="travel_logo" height="60" />
         </div>
         <Toolbar className={classes.toolbar}>
             { user ? (
